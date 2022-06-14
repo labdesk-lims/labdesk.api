604c49a9b8ab7cfab3eb8144768931cd34982845
Lutz Kogel <77008074+KOLUMA-HUB@users.noreply.github.com>
2022-06-14 08:24:21 +0200

    Error handling revised
    Error handling with text instead of code added.

--- index.php
+++ index.php
@@ -1,16 +1,6 @@
 <?php
 error_reporting(E_ERROR | E_PARSE);
 
-/* Applied error codes:
- * E01 - Establishing connection failed
- * E02 - Invalid unique identifier
- * E03 - Token unknown
- * E05 - Nothing to report
- * E06 - CUD failed
- * E07 - Unknown path
- * E08 - Primary is null
- */
-
 // Parse url including parameters
 function parse_url_detail($url){
     $parts = parse_url($url);
@@ -22,56 +12,67 @@ function parse_url_detail($url){
 
 try {
 	// Connect to database
-	$serverName = "???";
-	$connectionInfo = array("Database"=>"labdesk", "UID"=>"rest-api", "PWD"=>"???");
+	$serverName = "169.254.128.250\MSSQL";
+	$connectionInfo = array("Database"=>"labdesk", "UID"=>"rest-api", "PWD"=>"260779");
 	$con = sqlsrv_connect($serverName, $connectionInfo);
 
-	// Check if connection was established, otherwise warn
+	// Check if connection was established, otherwise throw error
 	if( !$con ) {
-		 throw new Exception('E01');
+		 throw new Exception('Establishing a database connection failed.');
 	}
 
 	// Request and parse url from server
 	$url = parse_url_detail($_SERVER['REQUEST_URI']);
 
-	// Get url parameters
-	$token = $url['query']['token'];
-	$api = $url['query']['api'];
-	$action = $url['query']['action'];
-	$title = $url['query']['title'];
-	$sap_matno = $url['query']['sap_matno'];
-	$sap_blocked = $url['query']['sap_blocked'];
-	$sap_additionals = $url['query']['sap_additionals'];
-	$id = $url['query']['id'];
-
+	// Get parameters from URL
+	$token = $url['query']['token']; // Token used for authentication
+	$api = $url['query']['api']; // Api to be used (e.g. material)
+	
+	// Validate request send
+	If ($token == Null Or $api == null) {
+		throw new Exception('Invalid request.');
+	}
+	
 	// Authorize http request
 	$stmt = sqlsrv_query( $con, 'SELECT * FROM api WHERE token = ?', array(&$token));
 	
-	// Check for valid unique identifier
+	// Check for valid unique identifier, otherwise throw error
 	if( !$stmt ) {
-		throw new Exception('E02');
+		throw new Exception('Invalid token.');
 	}
 	
 	// Check for valid token
 	If (strtolower(sqlsrv_fetch_array( $stmt, SQLSRV_FETCH_ASSOC)['token']) != $token) {
-		throw new Exception('E03');
+		throw new Exception('Token not known.');
 	}
 	
-	// Switch interface
 	switch($api) {
 		case 'material':
-			// Perform action
+			// Switch depending action chosen
+			$action = $url['query']['action'];
+			
+			If ($action == Null) {
+				throw new Exception('Action missing.');
+			}
+			
+			// Get material specific parameters for SQL request
+			$title = $url['query']['title'];
+			$sap_matno = $url['query']['sap_matno'];
+			$sap_blocked = $url['query']['sap_blocked'];
+			$sap_additionals = $url['query']['sap_additionals'];
+			$id = $url['query']['id'];
+			
 			switch ($action) {
 				case 'c':
 					$stmt = sqlsrv_query( $con, 'INSERT INTO material (title, sap_matno, sap_blocked, sap_additionals) VALUES(?, ?, ?, ?)', array(&$title, &$sap_matno, &$sap_blocked, &$sap_additionals));
 					if( $stmt === false ) {
-						 throw new Exception('E06');
+						 throw new Exception('Failed to insert dataset.');
 					}
 					break;
 				case 'r':
 					$stmt = sqlsrv_query( $con, 'SELECT * FROM material WHERE sap_matno = ?', array(&$sap_matno));
 					if( $stmt === false ) {
-						 throw new Exception('E05');
+						 throw new Exception('Failed to read dataset.');
 					}
 					while( $row = sqlsrv_fetch_array( $stmt, SQLSRV_FETCH_ASSOC) ) {
 						  $res[] = $row;
@@ -83,23 +84,27 @@ try {
 					If ($id == Null) { throw new Exception('E08'); }
 					$stmt = sqlsrv_query( $con, 'UPDATE material SET title = ?, sap_matno = ?, sap_blocked = ?, sap_additionals = ? WHERE id = ?', array(&$title, &$sap_matno, &$sap_blocked, &$sap_additionals, &$id));
 					if( $stmt === false ) {
-						 throw new Exception('E06');
+						 throw new Exception('Failed to update dataset.');
 					}
 					break;
 				case 'd':
 					If ($id == Null) { throw new Exception('E08'); }
 					$stmt = sqlsrv_query( $con, 'DELETE FROM material WHERE id = ?', array(&$id));
 					if( $stmt === false ) {
-						 throw new Exception('E06');
+						 throw new Exception('Failed to delete dataset.');
 					}
 					break;
+				default:
+					throw new Exception('Action unknown.');
 				sqlsrv_free_stmt($stmt);  
 			}
 			break;
+		default:
+			throw new Exception('API unknown.');
 	}
 	
 	sqlsrv_close($con); 
 } catch (Exception $e) {
-	echo $e->getMessage(), "\n";
+	echo '<b>Error:</b> '.$e->getMessage()."</br>";
 }
 ?>
